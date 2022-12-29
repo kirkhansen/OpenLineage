ae9a535e784db98f16d21aef48652c94517cbc11
Kirk Hansen <kirk.hansen@corteva.com>
2022-12-29 14:24:11 -0600

    add schema facet support

--- integration/dagster/openlineage/dagster/utils.py
+++ integration/dagster/openlineage/dagster/utils.py
@@ -15,23 +15,43 @@ from dagster import (
     EventRecordsFilter,
 )
 from dagster._core.events import DagsterEventType  # type: ignore
-from openlineage.client.run import InputDataset, OutputDataset, Dataset
+from openlineage.client.facet import SchemaDatasetFacet, SchemaField
+from openlineage.client.run import Dataset
 
 
 NOMINAL_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
 
-# TODO: should be able: int | None = None to glean this based on environment/DagsterInstance context
+# TODO: should be able to glean this based on environment/DagsterInstance context
 GRAPHQL_HOST = "http://localhost:3001/graphql"
 
 ASSET_NODE_QUERY = """
 query AssetNodes($pipelineSelector: PipelineSelector!) {
-  assetNodes(
-    pipeline: $pipelineSelector
-  ) {
+  assetNodes(pipeline: $pipelineSelector) {
     id
     ...assetPath
+    type {
+      name
+      displayName
+      description
+      metadataEntries {
+        ... on TableSchemaMetadataEntry {
+          label
+          schema {
+            columns {
+              name
+              type
+              description
+              constraints {
+                nullable
+                unique
+              }
+            }
+          }
+        }
+      }
+    }
     dependedBy {
-      asset{
+      asset {
         ...assetPath
       }
     }
@@ -68,22 +88,33 @@ def make_step_run_id() -> str:
 def make_step_job_name(pipeline_name: str, step_key: str) -> str:
     return f"{pipeline_name}.{step_key}"
 
+
+def _get_table_schema_facet(node_response: dict) -> dict:
+    """build up a SchemaDatasetFacet if a TableSchemaMetadataEntry is present on the asset"""
+
+    # should be one per node here
+    for metadata in node_response["type"]["metadataEntries"]:
+        if metadata["label"] == "schema":
+            schema = metadata["schema"]
+            schema["description"] = node_response["type"]["description"]
+            schema_fields = [SchemaField(name=column["name"], type=column["type"], description=column["description"]) for column in schema["columns"]]
+            schema_facet = SchemaDatasetFacet(fields=schema_fields)
+            return {"schema": schema_facet}
+    return {}
+
+
 def get_asset_record_dependencies(repository_name: str, repository_location: str, pipeline_name: str,) -> dict:
     """
     Hits graphql endpoint to fetch the asset records and their dependencies to
     add on to the dagster events for dataset definitions with ins/outs.
     """
-    # TODO:  filter out based on running pipelines ?
-    # pipeline_ids = running_pipelines.keys()
-    # TODO: cache grabbing asset nodes, or turn into single queries per asset?
-    # TODO: should inputs and outputs be openlineage Dataset objects? i think so. simplifies some things downstream, i think
     query_params = {"pipelineSelector": {"pipelineName": pipeline_name, "repositoryName": repository_name, "repositoryLocationName": repository_location}}
     assets_nodes_response = requests.post(GRAPHQL_HOST, json={"query": ASSET_NODE_QUERY, "variables": query_params}).json()
     asset_nodes = assets_nodes_response["data"]["assetNodes"]
     asset_node_lookup = {
             AssetKey(asset_node["assetKey"]["path"]): {
                 "input_datasets": [Dataset(namespace=repository_name, name=AssetKey(dep["asset"]["assetKey"]["path"]).to_python_identifier()) for dep in asset_node["dependencies"]],
-                "output_datasets": [Dataset(namespace=repository_name, name=AssetKey(asset_node["assetKey"]["path"]).to_python_identifier())]
+                "output_datasets": [Dataset(namespace=repository_name, name=AssetKey(asset_node["assetKey"]["path"]).to_python_identifier(), facets=_get_table_schema_facet(asset_node))]
             }
         for asset_node in asset_nodes
     }
